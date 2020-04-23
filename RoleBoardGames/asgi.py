854c6cb1ec7332b11ea638d4d3fdf2b4f8e51962
Juan Carlos <jucar251096@gmail.com>
2020-04-23 11:36:56 -0500

    Primer commit del proyecto

---
+++ RoleBoardGames/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for RoleBoardGames project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'RoleBoardGames.settings')
+
+application = get_asgi_application()
