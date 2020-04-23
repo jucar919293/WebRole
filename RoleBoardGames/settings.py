7c903122660ca975b4677b49ebff6ebd193590d4
Anita <jucar_1996@hotmail.com>
2020-04-23 12:16:40 -0500

    Entorno en windows

--- RoleBoardGames/settings.py
+++ RoleBoardGames/settings.py
@@ -74,10 +74,12 @@ WSGI_APPLICATION = 'RoleBoardGames.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+'default': {
+'ENGINE': 'django.db.backends.mysql',
+'NAME': 'dataflair',
+'USER': 'root',
+'PASSWORD': “” 
+}
 }
 
 
